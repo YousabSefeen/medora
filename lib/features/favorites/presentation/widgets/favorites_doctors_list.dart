7606c56834ebe4e728c280b4d7c2aaa2026c0e8e
Yousab <yousabsefeen@gmail.com>
2026-01-28 22:16:39 +0200

    feat: show empty state UI for favorites list

--- lib/features/favorites/presentation/widgets/favorites_doctors_list.dart
+++ lib/features/favorites/presentation/widgets/favorites_doctors_list.dart
@@ -1,17 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:medora/core/constants/app_strings/app_strings.dart';
+import 'package:medora/core/constants/common_widgets/content_unavailable_widget.dart'
+    show ContentUnavailableWidget;
 import 'package:medora/core/constants/common_widgets/error_retry_widget.dart'
     show ErrorRetryWidget;
 import 'package:medora/core/constants/common_widgets/sliver_loading%20_list.dart'
     show SliverLoadingList;
 import 'package:medora/core/enum/lazy_request_state.dart';
-import 'package:medora/features/shared/presentation/widgets/doctor_list_view.dart'
-    show DoctorListView;
 import 'package:medora/features/favorites/presentation/controller/cubit/favorites_cubit.dart'
     show FavoritesCubit;
 import 'package:medora/features/favorites/presentation/controller/states/favorites_states.dart'
     show FavoritesStates;
+import 'package:medora/features/shared/domain/entities/doctor_entity.dart'
+    show DoctorEntity;
+import 'package:medora/features/shared/presentation/widgets/doctor_list_view.dart'
+    show DoctorListView;
 
 class FavoritesDoctorsList extends StatefulWidget {
   const FavoritesDoctorsList({super.key});
@@ -48,7 +52,7 @@ class _FavoritesDoctorsListState extends State<FavoritesDoctorsList> {
           case LazyRequestState.loading:
             return const SliverLoadingList(height: 150);
           case LazyRequestState.loaded:
-            return DoctorListView(doctorList: state.favoritesDoctorsList);
+            return _buildFavoritesContent(state);
 
           case LazyRequestState.error:
             return ErrorRetryWidget(
@@ -61,4 +65,22 @@ class _FavoritesDoctorsListState extends State<FavoritesDoctorsList> {
       },
     );
   }
+
+  Widget _buildFavoritesContent(FavoritesStates state) {
+    if (state.favoritesDoctorsList.isEmpty) {
+      return _buildEmptyFavoritesView();
+    } else {
+      return _buildFavoritesListView(state.favoritesDoctorsList);
+    }
+  }
+
+  Widget _buildEmptyFavoritesView() => const SliverFillRemaining(
+    hasScrollBody: false,
+    child: ContentUnavailableWidget(
+      description: AppStrings.noFavoritesDoctorsFoundMessage,
+    ),
+  );
+
+  Widget _buildFavoritesListView(List<DoctorEntity> doctors) =>
+      DoctorListView(doctorList: doctors);
 }
